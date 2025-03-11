374dbe4a3eb6c84ad8a8e572f81fb42a78fd6f12
AliciaSchiochetSouza <alicia.souza@univille.br>
2025-03-10 22:00:24 -0300

    feat: revisao_alicia

---
+++ revisao_alicia/src/main/java/br/univille/Main.java
@@ -0,0 +1,13 @@
+package br.univille;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+
+        Cliente zezinho = new Cliente();
+        zezinho.setNome (" Zezinho");
+        Cliente mariazinha = new Cliente();
+        zezinho.setNome ("Mariazinha");
+
+    }
+}
