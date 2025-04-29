88f57410be300cd43783b592ed9f877db9dcc4c9
iwilliann <willianalves@gmail.com>
2025-04-28 21:27:44 -0300

    API REST e Controller Aluno

--- projfabsoftescolamusica/src/main/java/br/univille/projfabsoftescolamusica/controller/AlunoController.java
+++ projfabsoftescolamusica/src/main/java/br/univille/projfabsoftescolamusica/controller/AlunoController.java
@@ -5,15 +5,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import br.univille.projfabsoftescolamusica.entity.Aluno;
 import br.univille.projfabsoftescolamusica.service.AlunoService;
 
-@RestController
+@RestController // envia JSON e recebe JSON
 @RequestMapping("/api/v1/aluno") //talvez falte um s no alunos
 public class AlunoController {
 
@@ -37,5 +40,45 @@ public class AlunoController {
         }
         
         return ResponseEntity.badRequest().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Aluno> putAluno(@PathVariable long id, @RequestBody Aluno aluno){
+        if (id <= 0 || aluno == null){ // identificação do aluno nõa pode ser 0 ou menor, e aluno nõa pode estar em branco(null)
+            return ResponseEntity.badRequest().build();
+        }
+
+        var alunoAntigo = service.getByID(id); // procura se informação do aluno no banco está no banco
+        if (alunoAntigo == null){
+            return ResponseEntity.badRequest().build();
+        }
+
+        // pode utilizar a biblioteca Mapper, pra ser mais prático
+        alunoAntigo.setNome(aluno.getNome());
+        alunoAntigo.setEndereco(aluno.getEndereco());
+        alunoAntigo.setTelefone(aluno.getTelefone());
+        alunoAntigo.setEmail(aluno.getEmail());
+        alunoAntigo.setDataNascimento(aluno.getDataNascimento());
+        alunoAntigo.setCpf(aluno.getCpf());
+        alunoAntigo.setSexo(aluno.getSexo());
+
+        service.save(alunoAntigo);
+        return new ResponseEntity<Aluno>(alunoAntigo, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Aluno> deleteAluno(@PathVariable long id){
+        if(id <= 0){
+            return ResponseEntity.badRequest().build();
+        }
+
+        var alunoExcluido = service.getByID(id);
+        if(alunoExcluido == null){
+            return ResponseEntity.badRequest().build();
+        }
+
+        service.delete(id);
+
+        return new ResponseEntity<Aluno> (alunoExcluido, HttpStatus.OK);
     }
 }
--- projfabsoftescolamusica/src/main/java/br/univille/projfabsoftescolamusica/entity/Aluno.java
+++ projfabsoftescolamusica/src/main/java/br/univille/projfabsoftescolamusica/entity/Aluno.java
@@ -38,4 +38,54 @@ public class Aluno {
     public void setNome(String nome) {
         this.nome = nome;
     }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(int dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    
 }
